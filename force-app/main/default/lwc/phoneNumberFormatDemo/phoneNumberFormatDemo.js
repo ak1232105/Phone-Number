32183128fd5092f82df791c97589551923cd522b
ak1232105 <ak.1232105@outlook.com>
2024-07-08 13:29:05 +0530

    8 7 24 - 1

--- force-app/main/default/lwc/phoneNumberFormatDemo/phoneNumberFormatDemo.js
+++ force-app/main/default/lwc/phoneNumberFormatDemo/phoneNumberFormatDemo.js
@@ -1,44 +1,52 @@
 import { LightningElement, track, wire } from 'lwc';
 import getAllCountryCodes from '@salesforce/apex/CountryCodeController.getAllCountryCodes';
 import validatePhoneNumberApex from '@salesforce/apex/PhoneNumberController.validatePhoneNumber';
+
 export default class PhoneNumberFormatDemo extends LightningElement {
     @track countryCode;
-  @track phoneNumber;
-  @track errorMessage;
-  @track countryOptions = [];
+    @track phoneNumber;
+    @track errorMessage;
+    @track formattedNumber;
+    @track countryOptions = [];
 
-  @wire(getAllCountryCodes)
-  wiredCountryCodes({ error, data }) {
-    if (data) {
-      this.countryOptions = data.map(code => {
-        return { label: code, value: code };
-      });
-    } else if (error) {
-      // Handle error
-    }
-  }
-
-  handleCountryCodeChange(event) {
-    this.countryCode = event.target.value;
-  }
-
-  handlePhoneNumberChange(event) {
-    this.phoneNumber = event.target.value;
-  }
-
-  validatePhoneNumber() {
-    validatePhoneNumberApex({ phoneNumber: this.phoneNumber, countryCode: this.countryCode })
-      .then(result => {
-        if (result.isValid) {
-          this.errorMessage = '';
-        } else {
-          this.errorMessage = 'The phone number you provided is invalid.';
+    @wire(getAllCountryCodes)
+    wiredCountryCodes({ error, data }) {
+        if (data) {
+            this.countryOptions = data.map(code => {
+                return { label: code, value: code };
+            });
+        } else if (error) {
+            // Handle error
         }
-        this.template.querySelectorAll('lightning-input, lightning-combobox').forEach(input => input.reportValidity());
-      })
-      .catch(error => {
-        this.errorMessage = 'An error occurred while validating the phone number.';
-        this.template.querySelectorAll('lightning-input, lightning-combobox').forEach(input => input.reportValidity());
-      });
-  }
+    }
+
+    handleCountryCodeChange(event) {
+        this.countryCode = event.target.value;
+    }
+
+    handlePhoneNumberChange(event) {
+        this.phoneNumber = event.target.value;
+    }
+
+    validatePhoneNumber() {
+        // Extract the region code from the selected country code (e.g., "IN" from "IN (91)")
+        const regionCode = this.countryCode.split(' ')[0];
+        
+        validatePhoneNumberApex({ phoneNumber: this.phoneNumber, regionCode: regionCode })
+            .then(result => {
+                if (result.isValid) {
+                    this.errorMessage = '';
+                    this.formattedNumber = result.formattedNumber;
+                } else {
+                    this.errorMessage = 'The phone number you provided is invalid.';
+                    this.formattedNumber = '';
+                }
+                this.template.querySelectorAll('lightning-input, lightning-combobox').forEach(input => input.reportValidity());
+            })
+            .catch(error => {
+                this.errorMessage = 'An error occurred while validating the phone number.';
+                this.formattedNumber = '';
+                this.template.querySelectorAll('lightning-input, lightning-combobox').forEach(input => input.reportValidity());
+            });
+    }
 }
